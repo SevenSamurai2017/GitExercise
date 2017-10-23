629e28c6d625ebb8886f40b00f4508981d63f910
Armannpetur <armanna16@ru.is>
2017-10-23 23:08:03 +0000

    fill board ready

--- src/main/java/is/ru/TicTacToe/Board.java
+++ src/main/java/is/ru/TicTacToe/Board.java
@@ -8,6 +8,15 @@ public class Board {
     public Board(){
     	board = new char[3][3];
     }
-    
+    public void fillBoard(){
+    	char counter = '1';
+    	for (int i = 0; i < 3 ;i++) {
+    		for (int j = 0; j < 3 ;j++) {
+    			board[i][j] = counter;
+    			counter++;
+    		}
+    	}
+    }
+
 }
 
