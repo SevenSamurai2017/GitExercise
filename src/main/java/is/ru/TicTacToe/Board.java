329dda8c61a4d3f50dfd08f2a5623ff97fbed886
Armannpetur <armanna16@ru.is>
2017-10-23 23:10:48 +0000

    print board

--- src/main/java/is/ru/TicTacToe/Board.java
+++ src/main/java/is/ru/TicTacToe/Board.java
@@ -17,6 +17,14 @@ public class Board {
     		}
     	}
     }
+    public void printBoard(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }    
 
 }
 
