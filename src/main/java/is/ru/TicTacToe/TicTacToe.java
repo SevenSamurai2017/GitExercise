548fef45d8522790f3f88b518c5edf922ebc61a9
Arnar Pétursson <arnarpeturs@gmail.com>
2017-10-24 10:51:55 +0000

    Constructor for custom names added

--- src/main/java/is/ru/TicTacToe/TicTacToe.java
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -6,11 +6,22 @@ public class TicTacToe{
 	public Board b;
 	public int actions;
 
+	// Basic Game
 	public TicTacToe(){
 		b = new Board();
 		b.fillBoard();
 		player1 = new Player("Player 1", 'X');
         player2 = new Player("Player 2", 'O');
+        currentPlayer = player1;
+        actions = 0;
+	}
+
+	// Game with custom names(Player vs Player)
+	public TicTacToe(String p1Name, String p2Name){
+		b = new Board();
+        b.fillBoard();
+        player1 = new Player(p1Name, 'X');
+        player2 = new Player(p2Name, 'O');
         currentPlayer = player1;
         actions = 0;
 	}
