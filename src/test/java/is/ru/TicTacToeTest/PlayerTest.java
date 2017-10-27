115c8f89ef8a27fe241adf3ffd0c75fef054d099
Arnar Pétursson <arnarpeturs@gmail.com>
2017-10-27 15:24:12 +0000

    Tests for player updated

--- src/test/java/is/ru/TicTacToeTest/PlayerTest.java
+++ src/test/java/is/ru/TicTacToeTest/PlayerTest.java
@@ -4,33 +4,17 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class PlayerTest {
-/*
+
 	@Test
 	public void getNameTest() {
 		Player P1 = new Player("Manni", 'X');
 		assertEquals("Manni", P1.getName());
 	}
-	@Test
-	public void getNumberOfWinsTest() {
-		Player P1 = new Player("Manni", 'X');
-		assertEquals(0, P1.getNumberOfWins());
-	}
-	@Test
-	public void playerWonTest() {
-		Player P1 = new Player("Manni", 'X');
-		P1.playerWon();
-		assertEquals(1, P1.getNumberOfWins());
-	}
-	@Test
-	public void multipleWinsTest() {
-		Player P1 = new Player("Manni", 'X');
-		P1.playerWon();
-		P1.playerWon();
-		P1.playerWon();
-		P1.playerWon();
-		P1.playerWon();
 
-		assertEquals(5, P1.getNumberOfWins());
+	@Test
+	public void getMarkerTest(){
+		Player P1 = new Player("Manni", 'X');
+		assertEquals('X', P1.getMarker());	
 	}
-	*/
+
 }
