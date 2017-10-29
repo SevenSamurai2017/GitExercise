641e84b0ea5041ec372aac4a4484a692aee8f20e
audurr16 <audurr16@ru.is>
2017-10-29 13:24:42 +0000

    comments

--- src/main/java/is/ru/TicTacToe/TicTacToe.java
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -2,12 +2,20 @@ package is.ru.TicTacToe;
 
 import is.ru.TicTacToe.exceptions.*;
 
+/**
+*
+*
+*/
 public class TicTacToe {
 
     private Player player1, player2, currentPlayer;
     public Board board;
     private boolean isOver;
 
+    /**
+    *
+    *
+    */
     public TicTacToe(){
         board = new Board();
         player1 = new Player("Player1", PlayerSymbol.X);
@@ -16,14 +24,22 @@ public class TicTacToe {
         isOver = false;
     }
 
+    /**
+    * Changes current player
+    */
     public void changePlayer(){
         currentPlayer = currentPlayer == player1 ? player2 : player1;
     }
 
+    /**
+    * Marks move if game is not over
+    * @param move selected location for marker
+    * <return> returns marker
+    */
     public char makeMove(int move) throws AlreadyOccupiedException,
                                           BoundaryException,
                                           IllegalSymbolException{
-        checkForWinner();                                            
+        checkForWinner();
         Coordinates coord = DimensionMapper.getCoordinate(move);
         if(isOver){
             return board.get(coord.getRow(), coord.getColumn());
@@ -32,15 +48,27 @@ public class TicTacToe {
         return currentPlayer.getMarker();
     }
 
+    /**
+    *
+    * @param value The location where we want to get the value from
+    * <return> returns char value of location
+    */
     public char getBoardValue(int value) throws BoundaryException {
         Coordinates coord = DimensionMapper.getCoordinate(value);
         return board.get(coord.getRow(), coord.getColumn());
     }
 
+    /**
+    * <return> returns current player
+    */
     public String getCurrentPlayerName(){
         return currentPlayer.getName();
     }
 
+    /**
+    *
+    * <return> returns
+    */
     public boolean checkForWinner() throws BoundaryException{
         if(!checkDiagonal()){
             if(!checkHorizontal()){
@@ -53,6 +81,10 @@ public class TicTacToe {
         return true;
     }
 
+    /**
+    *
+    *
+    */
     private boolean checkVertical() throws BoundaryException{
         for(int i = 0; i < 3; i++){
             if(board.get(i, 0) == board.get(i, 1) && board.get(i, 0) == board.get(i, 2)){
@@ -65,6 +97,10 @@ public class TicTacToe {
         return false;
     }
 
+    /**
+    *
+    *
+    */
     private boolean checkHorizontal() throws BoundaryException{
         for(int i = 0; i < 3; i++){
             if(board.get(0, i) == board.get(1, i) && board.get(0, i) == board.get(2, i)){
@@ -77,6 +113,10 @@ public class TicTacToe {
         return false;
     }
 
+    /**
+    *
+    *
+    */
     private boolean checkDiagonal() throws BoundaryException{
         if(board.get(0, 0) == board.get(1, 1) && board.get(1, 1) == board.get(2, 2)) {
             if(board.get(0, 0) != PlayerSymbol.X && board.get(0, 0) != PlayerSymbol.O){
