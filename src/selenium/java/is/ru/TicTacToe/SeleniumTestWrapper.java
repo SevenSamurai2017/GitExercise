0de9a5077c1595421a27fd4556fe8d5db7f1bf0a
arnarpeturs <arnarp16@ru.is>
2017-10-29 15:58:00 +0000

    travis sauce and selenium test

--- src/selenium/java/is/ru/TicTacToe/SeleniumTestWrapper.java
+++ src/selenium/java/is/ru/TicTacToe/SeleniumTestWrapper.java
@@ -2,22 +2,38 @@ package is.ru.TicTacToe;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.net.Urls;
+import java.net.MalformedURLException;
+import org.openqa.selenium.WebDriver; 
+import org.openqa.selenium.remote.DesiredCapabilities; 
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import java.net.URL;
+import java.lang.*;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 public abstract class SeleniumTestWrapper {
-  static ChromeDriver driver;
+  //static ChromeDriver driver;
   static String baseUrl;
   static String port;
+  static WebDriver driver;
+
+  public static final String USERNAME = "arnarpeturs";
+  public static final String ACCESS_KEY = "d1bd2ddd-bd70-471e-a49e-204c0435fb93";
+  public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
 
   @BeforeClass
-  public static void openBrowser() {
-    driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+  public static void openBrowser() throws MalformedURLException {
+  /*  driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);*/
 
-    port = System.getenv("PORT");
+  DesiredCapabilities caps = DesiredCapabilities.chrome();
+  /*caps.setCapability("platform", "Windows XP");
+  caps.setCapability("version", "43.0");*/
+  driver = new RemoteWebDriver(new URL(URL), caps);
+
+  port = System.getenv("PORT");
     if (port == null) {
         port = "4567";
     }
--- src/selenium/java/is/ru/TicTacToe/TestTicTacToe.java
+++ src/selenium/java/is/ru/TicTacToe/TestTicTacToe.java
@@ -1,9 +1,9 @@
 package is.ru.TicTacToe;
 
-
+import java.lang.NullPointerException;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-
+import java.net.MalformedURLException;
 import org.openqa.selenium.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -11,17 +11,17 @@ import org.openqa.selenium.WebElement;
 public class TestTicTacToe extends SeleniumTestWrapper {
   @Test
   public void testTitleMatches() {
-    driver.get(baseUrl);
+    driver.get("https://shielded-dawn-85701.herokuapp.com/");
     assertEquals("TicTacToe", driver.getTitle());
   }
 
   @Test
-  public void testOneClick() throws Exception {
-    driver.get(baseUrl);
-    Thread.sleep(2000);
+  public void testOneClick() throws MalformedURLException {
+    driver.get("https://shielded-dawn-85701.herokuapp.com/");
+    //Thread.sleep(2000);
     WebElement input = driver.findElement(By.id("btn1"));
     input.click();
-    Thread.sleep(5000);
+    //Thread.sleep(5000);
     assertEquals("X", driver.findElement(By.id("btn1")).getAttribute("value"));
   }
 }
